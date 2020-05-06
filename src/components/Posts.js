23d1f7256b3458d5ec584b2b0d10d075598b71f3
akoeth-portfolio <dukewellington61@googlemail.com>
2020-05-06 18:47:39 +0200

    connect explained

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -46,7 +46,14 @@ const mapStateToProps = (state) => ({
 });
 
 // connect function connects Posts component to the redux store
+// It provides its connected component with the pieces of the data it needs from the store,
+// and the functions it can use to dispatch actions to the store.
 // mapStateToProps: state from redux store is maped to our component props
 // apart from whatever props Posts component was receiving, it will now recieve an additional prob called
 // posts which reflects the posts in the redux store
+// we are exporting the combination of a component that is connected to the store
+// Any changes to the state will be reflected in the component because it is “connected”
+// to mapStateToProps and that information is now made available to the component through a prop.
+// As the first argument passed in to connect, mapStateToProps is used for selecting the part of the data
+// from the store that the connected component needs.
 export default connect(mapStateToProps, { fetchPosts })(Posts);
